bea244cf644e73cc142ff3ef638cf5a3cb9d7311
sheldoncoates <sjrcoates@sympatico.ca>
2018-11-24 15:07:59 -0500

    got data converting and saving

--- Front-end/server.js
+++ Front-end/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 var path = require('path');
 const port = 3000
-app.use(express.static('www'))
+app.use(express.static('www'));
+var fs = require('fs');
+var filename = "../Back-End/sample_collection_data.json";
 
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname + '/index.html'));
@@ -15,3 +17,20 @@ app.listen(port, (err) => {
 
   console.log(`server is listening on ${port}`)
 })
+
+// fs.exists(filename, function(exists) {
+//   if (exists) {
+//     fs.stat(filename, function(err, stats) { 
+//       if (stats.isDirectory()) {
+//         console.log(filename + ": is a directory");
+//       } else {
+//         // do something with file
+//         console.log(filename);
+//         var myJSONdata = JSON.stringify("hello");
+//         fs.writeFileSync(filename, myJSONdata); //default: 'utf8'
+//       }
+//     });
+//   } else {
+//     console.log(filename + ": no such file");
+//   }
+// });
--- Front-end/www/index.js
+++ Front-end/www/index.js
@@ -1,4 +1,5 @@
 const { Chirp } = ChirpConnectSDK;
+var processData;
 
 var processChirp = function (){
   Chirp({
@@ -12,7 +13,7 @@ var processChirp = function (){
       }
     }
   }).then(sdk => {
-    sdk.send([0, 1, 2, 3]);
+    sdk.send("hello");
     sdk.stop();
   }).catch(console.error)
 }
@@ -26,11 +27,13 @@ var receiveChirp = function(){
     onReceived: data => {
       if (data.length > 0) {
         console.log(data);
+        processData = data;
+        convertDataAndSave();
         stopSDK();
       }
     }
   }).then(sdk => {
-    //do nothing for now maybe make the json here
+    //do nothing for now maybe make the json here    
   }).catch(console.error)
 }
 var stopSDK = function(){
@@ -42,4 +45,35 @@ var stopSDK = function(){
   }).then(sdk => {
     sdk.stop();
   }).catch(console.error)
+}
+
+var convertDataAndSave = function(){
+  Chirp({
+    key: 'CFaaF6C954bA8ddb5f5CFDeBD',
+    onStateChanged: (previous, current) => {
+      console.log(current);
+    }
+  }).then(sdk => {
+    console.log(processData);
+    console.log(sdk.asString(processData));
+    console.log(hex2a(sdk.asString(processData)));
+    var ASCIIData = hex2a(sdk.asString(processData));
+    var splitData = ASCIIData.split(",");
+    var transaction = {
+          "date":splitData[0],
+          "type":splitData[1],
+          "partner":2321442142,
+          "amount":50
+    };
+    localStorage.setItem('storeObj', JSON.stringify(transaction));
+    
+  }).catch(console.error)
+}
+
+var hex2a = function (hexx) {
+  var hex = hexx.toString();//force conversion
+  var str = '';
+  for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
+      str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
 }
