9d211f0780ab33b355a12e5aae2e5b587c20c772
sheldoncoates <sjrcoates@sympatico.ca>
2018-11-24 12:59:08 -0500

    distinguished sending and receiving

--- Front-end/www/index.js
+++ Front-end/www/index.js
@@ -1,18 +1,45 @@
 const { Chirp } = ChirpConnectSDK;
 
-var sendChirp = function (){
+var processChirp = function (){
   Chirp({
     key: 'CFaaF6C954bA8ddb5f5CFDeBD',
     onStateChanged: (previous, current) => {
-      console.log(current)
+      console.log(current);
     },
     onReceived: data => {
       if (data.length > 0) {
-        console.log(data)
+        console.log(data);
       }
     }
   }).then(sdk => {
     sdk.send([0, 1, 2, 3]);
+    sdk.stop();
   }).catch(console.error)
 }
 
+var receiveChirp = function(){
+  Chirp({
+    key: 'CFaaF6C954bA8ddb5f5CFDeBD',
+    onStateChanged: (previous, current) => {
+      console.log(current);
+    },
+    onReceived: data => {
+      if (data.length > 0) {
+        console.log(data);
+        stopSDK();
+      }
+    }
+  }).then(sdk => {
+    //do nothing for now maybe make the json here
+  }).catch(console.error)
+}
+var stopSDK = function(){
+  Chirp({
+    key: 'CFaaF6C954bA8ddb5f5CFDeBD',
+    onStateChanged: (previous, current) => {
+      console.log(current);
+    },
+  }).then(sdk => {
+    sdk.stop();
+  }).catch(console.error)
+}
