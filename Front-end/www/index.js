e2a04ae3c004847e341aa9c583f3f768871d8c3f
sheldoncoates <sjrcoates@sympatico.ca>
2018-11-24 16:23:04 -0500

    fixes maybe

--- Front-end/www/index.js
+++ Front-end/www/index.js
@@ -31,12 +31,23 @@ var receiveChirp = function () {
       if (data.length > 0) {
         console.log(data);
         processData = data;
-        convertDataAndSave();
+        var ASCIIData = hex2a(sdk.asString(processData));
+        var splitData = ASCIIData.split(",");
+        if (splitData[0] == "1") {
+          //reduce balance by amount sent
+          console.log("payment confirmed");
+        } else if (splitData[0] == "0") {
+          //cancel/do nothing 
+          console.log("payment cancelled");
+        }else{
+          convertDataAndSave();
+        }
         //stopSDK();
       }
     }
   }).then(sdk => {
-    //do nothing for now maybe make the json here    
+    //do nothing for now maybe make the json here 
+
   }).catch(console.error)
 }
 var stopSDK = function () {
@@ -62,15 +73,6 @@ var convertDataAndSave = function () {
     console.log(hex2a(sdk.asString(processData)));
     var ASCIIData = hex2a(sdk.asString(processData));
     var splitData = ASCIIData.split(",");
-    if(splitData[0]=="1"){
-        //reduce balance by amount sent
-        console.log("payment confirmed");
-        sdk.stop();
-    }else if(splitData[0]=="0"){
-      //cancel/do nothing 
-      console.log("payment cancelled");
-      sdk.stop();
-    }
     //date: is the current time stamp, type: always received in this case, wallet_id: id for user wallet, amount: is amount $
     if (myWallet_id == splitData[0]) {
       transaction += {
@@ -82,7 +84,7 @@ var convertDataAndSave = function () {
       localStorage.setItem('transaction', JSON.stringify(transaction));
       sdk.send("1");
       sdk.stop();
-    }else{
+    } else {
       console.log("wrong phone number");
       sdk.send("0");
       sdk.stop();
