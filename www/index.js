36a5a962fc1aba92aa08ba701468ff87075e6a9a
sheldoncoates <sjrcoates@sympatico.ca>
2018-11-24 05:00:19 -0500

    fixed js code and made it look nizeeeeee

--- www/index.js
+++ www/index.js
@@ -1,15 +1,18 @@
 const { Chirp } = ChirpConnectSDK;
 
-Chirp({
-  key: 'CFaaF6C954bA8ddb5f5CFDeBD',
-  onStateChanged: (previous, current) => {
-    console.log(current)
-  },
-  onReceived: data => {
-    if (data.length > 0) {
-      console.log(data)
+var sendChirp = function (){
+  Chirp({
+    key: 'CFaaF6C954bA8ddb5f5CFDeBD',
+    onStateChanged: (previous, current) => {
+      console.log(current)
+    },
+    onReceived: data => {
+      if (data.length > 0) {
+        console.log(data)
+      }
     }
-  }
-}).then(sdk => {
-  sdk.send([0, 1, 2, 3]);
-}).catch(console.error)
+  }).then(sdk => {
+    sdk.send([0, 1, 2, 3]);
+  }).catch(console.error)
+}
+
