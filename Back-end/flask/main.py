06d214e7d62516c326e04220e9e0df54ae5fa40c
Andrew Nash <33398082+andrewnash@users.noreply.github.com>
2018-11-25 02:24:04 -0330

    yes

--- Back-end/flask/main.py
+++ Back-end/flask/main.py
@@ -265,8 +265,8 @@ def create_user():
     finally:
         client.close()
     
-    user = {"wallet_id": str(request.data["wallet_id"].decode("utf-8")),
-            "password": str(request.data["password"].decode("utf-8")),
+    user = {"wallet_id": int(request.data["wallet_id"]),
+            "password": int(request.data["password"]),
             "balance": 0,
             "in_app_transactions": [],
             "out_app_transactions": [] }
