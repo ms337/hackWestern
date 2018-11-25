03b884757ee47e391138d2aa7d099f1b9d1510b6
Andrew Nash <33398082+andrewnash@users.noreply.github.com>
2018-11-24 20:44:43 -0330

    no debug

--- Back-end/flask/main.py
+++ Back-end/flask/main.py
@@ -48,5 +48,5 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='35.231.228.196', port=80, debug=True)
+    app.run(host='0.0.0.0')
 # [END gae_python37_app]
