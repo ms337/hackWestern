ffe32b62b802e5088156e189e87378001c711f21
Andrew Nash <33398082+andrewnash@users.noreply.github.com>
2018-11-25 04:28:08 -0330

    DSADA

--- Back-end/flask/main.py
+++ Back-end/flask/main.py
@@ -234,7 +234,6 @@ def test():
 # create a user  phone# password
 @app.route('/create_user', methods=['POST'])
 def create_user():
-    return Response(str(request))
     try:
         client = MongoClient('mongodb://admin:admin@hackwestern-shard-00-00-4qcqm.gcp.mongodb.net:27017,hackwestern-shard-00-01-4qcqm.gcp.mongodb.net:27017,hackwestern-shard-00-02-4qcqm.gcp.mongodb.net:27017/test?ssl=true&replicaSet=hackWestern-shard-0&authSource=admin')
         db = client['Dolphin']
@@ -245,7 +244,7 @@ def create_user():
         client.close()
     
     user = {"wallet_id": "sdklnasdas",
-            "password": request.form['password'],
+            "password": request['password'],
             "balance": 0,
             "in_app_transactions": [],
             "out_app_transactions": [] }
